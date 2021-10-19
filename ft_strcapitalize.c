c87b79bc89fee85386c7c1e678434f0df9b138a7
ekmbjh <74845690+ekmbjh@users.noreply.github.com>
2021-10-19 17:59:55 +0900

    Add files via upload

---
+++ ft_strcapitalize.c
@@ -0,0 +1,28 @@
+char	*ft_strcapitalize(char *str)
+{ 
+	int		i;
+	char	a;
+
+	i = 1;
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
+	while (str[i] != '\0')
+	{
+		a = str[i - 1];
+		if (!((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z')))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				if (!(str[i - 1] >= '0' && str[i - 1] <= '9'))
+					str[i] = str[i] - 32;
+		}
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			if (str[i - 1] >= '0' && str[i - 1] <= '9')
+				str[i] = str[i] + 32;
+		if ((a >= 'A' && a <= 'Z') && (str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] = str[i] + 32;
+		if ((a >= 'a' && a <= 'z') && (str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] = str[i] + 32;
+		i++;
+	}
+	return (str);
+}
