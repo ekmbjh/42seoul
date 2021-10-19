72262758998db4b118a30d7ea79b9c57128296cd
ekmbjh <74845690+ekmbjh@users.noreply.github.com>
2021-10-19 18:28:24 +0900

    Add files via upload

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,27 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	n;
+	int	result;
+
+	i = 0;
+	n = 1 ;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			n *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return (result * n);
+}
