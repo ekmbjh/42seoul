cf64339f4aad22b33329129f666655a3d24f7f4f
Junghun Baek <junbaek@c5r3s7.42seoul.kr>
2021-10-24 17:22:37 +0900

    1

--- rush02/main.c
+++ rush02/main.c
@@ -1,33 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junbaek <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/24 16:58:24 by junbaek           #+#    #+#             */
+/*   Updated: 2021/10/24 16:58:31 by junbaek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
-void ERROR()
+void	ERROR(void)
 {
-	char *str;
+	char	*str;
 
 	str = "ERROR!";
 	write(1, str, 6);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	char *str[2];
-	long long text_size;
+	char		*str[2];
+	long long	text_size;
 
-	if(argc != 2 && argc != 3)
+	if (argc != 2 && argc != 3)
 	{
 		ERROR();
 		return (0);
 	}
 	text_size = size_file();
-	if(argc == 2)
+	if (argc == 2)
 	{
 		str[0] = argv[1];
 		read_dict(str[0], text_size);
 	}
-	else if(argc == 3)
+	else if (argc == 3)
 	{
 		str[0] = argv[1];
 		str[1] = argv[2];
--- rush02/size_file.c
+++ rush02/size_file.c
@@ -3,16 +3,15 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-
-long long int size_file(void)
+long long int	size_file(void)
 {
-	long long int size;
-	char buff[100];
-	int fd;
-	long long int cnt;
+	long long int	size;
+	char			buff[100];
+	int				fd;
+	long long int	cnt;
 
 	size = 0;
-	if(0 < (fd = open("dict.txt", O_RDONLY)))
+	if (0 < (fd = open("dict.txt", O_RDONLY)))
 	{
 		while (0 < (cnt = read(fd, buff, 100)))
 		{
@@ -24,7 +23,7 @@ long long int size_file(void)
 		return (-1);
 }
 
-int main(void)
+int	main(void)
 {
-	printf("%lld",size_file());
+	printf("%lld", size_file());
 }
