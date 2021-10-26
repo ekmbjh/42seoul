7b026895c989a5e1db1bda9731ba31b516aa0a29
ekmbjh <74845690+ekmbjh@users.noreply.github.com>
2021-10-26 21:34:26 +0900

    Add files via upload

--- c08/ex04/ft_strs_to_tab.c
+++ c08/ex04/ft_strs_to_tab.c
@@ -14,4 +14,52 @@
 
 #include "ft_stock_str.h"
 
+int						ft_strlen(char *str)
+{
+	int					i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char					*ft_copy_str(char *src)
+{
+	char				*dst;
+	int					i;
+
+	dst = (char *)malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (dst == NULL)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
 struct s_stock_str		*ft_strs_to_tab(int ac, char **av)
+{
+	struct s_stock_str	*arr;
+	int					i;
+
+	i = 0;
+	arr = malloc((ac + 1) * sizeof(t_stock_str));
+	if (arr == 0)
+		return (NULL);
+	while (i < ac && av[i])
+	{
+		arr[i].size = ft_strlen(av[i]);
+		arr[i].str = av[i];
+		arr[i].copy = ft_copy_str(av[i]);
+		i++;
+	}
+	arr[i].str = 0;
+	return (arr);
+}
--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -12,6 +12,66 @@
 
 #include <unistd.h>
 
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	int				i;
+	int				digits_rev[1000];
+	unsigned int	unb;
+
+	if (nb == 0)
+	{
+		ft_putchar('0');
+		return ;
+	}
+	unb = nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		unb = -nb;
+	}
+	i = 0;
+	while (unb > 0)
+	{
+		digits_rev[i] = unb % 10;
+		unb /= 10;
+		i++;
+	}
+	while (i > 0)
+		ft_putchar(digits_rev[--i] + '0');
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
 void	ft_show_tab(struct s_stock_str *par)
 {
+	int i;
+
+	i = 0;
+	while (par[i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
+	}
 }
