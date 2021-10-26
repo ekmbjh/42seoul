fa99d7820c3951aad3680922743a793cec4a5b90
ekmbjh <74845690+ekmbjh@users.noreply.github.com>
2021-10-27 01:26:33 +0900

    Add files via upload

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -10,22 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z' )
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
 char	*ft_strcapitalize(char *str)
 {
-	int	x;	
+	int	x;
 	int	y;
 
 	x = 0;
 	y = 1;
 	ft_strlowcase(str);
-	 while (str[x] != '\0')
-		 {
+	while (str[x] != '\0')
+	{
 		if (str[x] >= 'a' && str[x] <= 'z')
-				{
-					if (y == 1)
-						str[x] -= 32;
-					y = 0;
-				}
+		{
+			if (y == 1)
+			{
+				str[x] -= 32;
+				y = 0;
+			}
+		}
 		else if (str[x] >= '0' && str [x] <= '9')
 			y = 0;
 		else
