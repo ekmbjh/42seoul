65c7c27438c2e936b2c2029525d12d10e3a9345d
hykcg <ekmbjh@naver.com>
2021-12-21 14:10:03 +0900

    12211410

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,27 +6,13 @@
 /*   By: junbaek <junbaek@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 15:29:02 by junbaek           #+#    #+#             */
-/*   Updated: 2021/12/21 09:55:51 by junbaek          ###   ########.fr       */
+/*   Updated: 2021/12/21 14:09:23 by junbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	**ft_malloc_error(char **tab)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-	return (NULL);
-}
-
-static unsigned int	ft_get_nb_strs(char const *s, char c)
+static unsigned int	ft_countstr(char const *s, char c)
 {
 	unsigned int	i;
 	unsigned int	cnt_str;
@@ -67,20 +53,26 @@ static void	ft_get_next_str(char **next_str, unsigned int *next_str_len,
 	}
 }
 
-char	**ft_split(char const *s, char c)
+static char	**ft_malloc_error(char **tab)
 {
-	char			**tab;
-	char			*next_str;
-	unsigned int	next_str_len;
-	unsigned int	cnt_str;
 	unsigned int	i;
 
-	if (!s)
-		return (NULL);
-	cnt_str = ft_get_nb_strs(s, c);
-	tab = (char **)malloc(sizeof(char *) * (cnt_str + 1));
-	if (!tab)
-		return (NULL);
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
+}
+
+static char **input_tab(char **tab, char const *s, char c, unsigned int cnt_str)
+{
+	char			*next_str;
+	unsigned int	next_str_len;
+	unsigned int	i;
+
 	i = 0;
 	next_str = (char *)s;
 	next_str_len = 0;
@@ -94,5 +86,20 @@ char	**ft_split(char const *s, char c)
 		i++;
 	}
 	tab[i] = NULL;
+	return (tab);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char			**tab;
+	unsigned int	cnt_str;
+
+	if (!s)
+		return (NULL);
+	cnt_str = ft_countstr(s, c);
+	tab = (char **)malloc(sizeof(char *) * (cnt_str + 1));
+	if (!tab)
+		return (NULL);
+	tab = input_tab(tab, s, c, cnt_str);
 	return (tab);
 }
