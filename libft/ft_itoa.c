184671a0202026320de3bdfcd44f75c55e7ffbea
hykcg <ekmbjh@naver.com>
2021-12-21 12:09:38 +0900

    12211209

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: junbaek <junbaek@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 12:39:42 by junbaek           #+#    #+#             */
-/*   Updated: 2021/12/21 10:09:17 by junbaek          ###   ########.fr       */
+/*   Updated: 2021/12/21 11:07:44 by junbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_countsize(long int n)
+static int	ft_countsize(long int n)
 {
 	if (n < 0)
 		return (1 + ft_countsize(-n));
@@ -22,7 +22,7 @@ int	ft_countsize(long int n)
 		return (1 + ft_countsize(n / 10));
 }
 
-void	ft_convbase(long int n, char *number, long int i)
+static void	ft_convbase(long int n, char *number, long int i)
 {
 	if (n < 0)
 	{
